aafec7c55f1106e3b226512a09198deb2121bd36
suumquique <lebedenko.fa@edu.spbstu.ru>
2021-03-28 15:57:51 +0300

    Add file opening and corresponding checks We open the file in binary format for reading and check if it opened correctly. If yes, we check if the file is not empty (whether its length is sufficient to contain bytes with information about the file and bytes of the palette with encrypted information about the length of the message)

--- main.cpp
+++ main.cpp
@@ -7,6 +7,23 @@ using namespace std;
 
 // Количество байт в формате .bmp, в которые записывается информация о файле, а не цвета пикселей
 #define INFORMATION_BYTES_COUNT 54
+// Количество бит в байте
+#define BITS_IN_BYTE 8
+/* Количество бит, выделенных на информацию о длине скрытого сообщения (в битах).
+* Учитываем то, что на 1 бит информации требуется 1 байт для ее скрытия (или на 2 бита один байт) */
+#define MESSAGE_LENGTH_INFORMATION_BITS_COUNT 32
+
+size_t getFileLength(ifstream& file) {
+
+	// Сдвигаем указатель на конец файла
+	file.seekg(0, ios_base::end);
+	// Узнаем, на каком месте стоит указатель
+	size_t length_in_bites = file.tellg();
+	// Сдвигаем указатель назад в начало файла
+	file.seekg(0, ios_base::beg);
+
+	return length_in_bites;
+}
 
 void main(void) {
 	// Устанавливаем русскую кодировку в консоли
@@ -14,14 +31,27 @@ void main(void) {
 	SetConsoleCP(1251);
 
 	wstring filePath;
+	wstring neededFileExtension = L".bmp";
 	cout << "Введите путь к изображению, в которое будет помещено зашифрованное сообщение: ";
 	getline(wcin, filePath);
-	wstring neededFileExtension = L".bmp";
-	
+
+
+	// Проверяем, заканчивается ли строка, содержащая путь к файлу, на его расширение - .bmp
 	if (filePath.compare(filePath.length() - neededFileExtension.length(), neededFileExtension.length(), neededFileExtension)) {
 		cout << "Неверное расширение файла. Требуется файл формата .bmp" << endl;
 		return;
 	}
 
+	ifstream binaryFile;
+	binaryFile.open(filePath, ios::binary);
+	if (!binaryFile.is_open()) {
+		cout << "Ошибка: файл не открылся: неверный путь к файлу, либо доступ к нему ограничен." << endl;
+		return;
+	}
 
+	if (getFileLength(binaryFile) <= INFORMATION_BYTES_COUNT * BITS_IN_BYTE + MESSAGE_LENGTH_INFORMATION_BITS_COUNT * BITS_IN_BYTE) {
+		cout << "Ошибка: файл скорее всего пуст, или практически все его содержимое удалено. Используйте другой BMP-файл для записи скрытого сообщения" << endl;
+		return;
+	}
+	
 }
